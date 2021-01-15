5da623a7e5ea15aadec806a4c059bcac516c22dc
kaizhe <derek0405@gmail.com>
2021-01-15 00:39:09 +0000

    fmt
    Signed-off-by: kaizhe <derek0405@gmail.com>

--- pkg/types/container.go
+++ pkg/types/container.go
@@ -1,10 +1,10 @@
 package types
 
 type Container struct {
-	ContainerID string `json:"ID"`
-	Created string `json:"Created"`
-	Image string `json:"Image"`
-	Config ContainerConfig `json:"Config"`
+	ContainerID string          `json:"ID"`
+	Created     string          `json:"Created"`
+	Image       string          `json:"Image"`
+	Config      ContainerConfig `json:"Config"`
 }
 
 type ContainerConfig struct {
